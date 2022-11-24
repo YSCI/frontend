374dea0d5371ef3c7fdeeda2c70a3d0401639cf0
haykAbrahamya <abrahamyan.hayk.03@gmail.com>
2022-11-24 08:32:57 +0400

    fix: fix education basis

--- src/constants/educationBasis.js
+++ src/constants/educationBasis.js
@@ -2,26 +2,26 @@ export const educationBasis = [
   {
     id: 1,
     label: 'Հիմնական կրթություն',
-    value: 0
+    value: 1
   },
   {
     id: 2,
     label: 'Միջնակարգ կրթություն',
-    value: 1
+    value: 2
   },
   {
     id: 3,
     label: 'Նախնական մասնագիտական',
-    value: 2
+    value: 3
   },
   {
     id: 4,
     label: 'Միջին մասնագիտական',
-    value: 3
+    value: 4
   },
   {
     id: 5,
     label: 'Բարձրագույն մասնագիտական',
-    value: 4
-  },
-]
+    value: 5
+  }
+];
